05b0765fff98820df071569697daa8696e66473f
rdgr1 <rodgerdias6@gmail.com>
2024-07-30 14:52:29 -0300

    Criando Rota PUT

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -20,3 +20,12 @@ def create_user(user: UserSchema):
 @app.get('/users/', response_model=UserList)
 def read_users():
     return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=user_id)
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
