1ca2d03cf1d0c576acb76906756cda4f0f5a0518
rdgr1 <rodgerdias6@gmail.com>
2024-07-30 14:02:10 -0300

    Teste de Post e Get

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,5 @@
 from http import HTTPStatus
 
-""" def test_read_root_deve_retornar_ok_e_ola_mundo(client):
-    response = client.get('/')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá Mundo'} 
-"""
-
 
 def test_create_user(client):
     response = client.post(
@@ -14,16 +7,19 @@ def test_create_user(client):
         json={
             'username': 'testusername',
             'email': 'test@test.com',
-            'id': 1,
+            'password': 'secret',
         },
     )
-
     assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'testusername',
+        'email': 'test@test.com',
+        'id': 1,
+    }
 
 
 def test_read_users(client):
     response = client.get('/users/')
-
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
         'users': [
