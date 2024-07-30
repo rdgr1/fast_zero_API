bcd2f4def71e55f8e42d63f0ae3a311704b08692
rdgr1 <rodgerdias6@gmail.com>
2024-07-30 15:26:57 -0300

    Metodo Post

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -25,7 +25,6 @@ def read_users():
 @app.put('/users/{user_id}', response_model=UserPublic)
 def update_user(user_id: int, user: UserSchema):
     user_with_id = UserDB(**user.model_dump(), id=user_id)
-
     database[user_id - 1] = user_with_id
 
     return user_with_id
