852cf1d5a9995251ea97b6145a5641b856256bdb
rdgr1 <rodgerdias6@gmail.com>
2024-08-02 13:19:55 -0300

    Migrations Fix

--- migrations/env.py
+++ migrations/env.py
@@ -3,15 +3,16 @@ from logging.config import fileConfig
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
-from alembic import context
-
 from fast_zero.models import table_registry
 from fast_zero.settings import Settings
 
+from alembic import context
+
+settings = Settings()
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
 config = context.config
 config.set_main_option('sqlalchemy.url', Settings().DATABASE_URL)
-
-
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
