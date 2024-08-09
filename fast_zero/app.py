5271d7b26d3c8aa3fa15cb093b658a1cfde7f7cc
Rodger Dias | BRASFORT <rodger.dias@brasfort.com.br>
2024-08-09 13:12:26 -0300

    Atualizando endpoints para user o banco de dados real

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -6,9 +6,10 @@ from sqlalchemy.orm import Session
 
 from fast_zero.database import get_session
 from fast_zero.models import User
-from fast_zero.schemas import UserList, UserPublic, UserSchema, Message
+from fast_zero.schemas import Message, UserList, UserPublic, UserSchema
 
 app = FastAPI()
+
 
 @app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
 def create_user(user: UserSchema, session: Session = Depends(get_session)):
@@ -39,6 +40,7 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
 
     return db_user
 
+
 @app.get('/users/', response_model=UserList)
 def read_users(
     skip: int = 0, limit: int = 100, session: Session = Depends(get_session)
@@ -46,13 +48,22 @@ def read_users(
     users = session.scalars(select(User).offset(skip).limit(limit)).all()
     return {'users': users}
 
+
+@app.get('/users/{user_id}', response_model=UserPublic)
+def read_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.id == user_id))
+    if not db_user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
+        )
+    return db_user
+
+
 @app.put('/users/{user_id}', response_model=UserPublic)
-def update_users(
+def update_user(
     user_id: int, user: UserSchema, session: Session = Depends(get_session)
 ):
-    db_user = session.scalar(
-        select(User).where(User.id == user_id)
-    )
+    db_user = session.scalar(select(User).where(User.id == user_id))
     if not db_user:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
@@ -67,19 +78,16 @@ def update_users(
 
     return db_user
 
+
 @app.delete('/users/{user_id}', response_model=Message)
-def delete_users(
-    user_id: int, session: Session = Depends(get_session)
-):
-    db_user = session.scalar(
-        select(User).where(User.id == user_id)
-    )
+def delete_user(user_id: int, session: Session = Depends(get_session)):
+    db_user = session.scalar(select(User).where(User.id == user_id))
     if not db_user:
         raise HTTPException(
             status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
-    
+
     session.delete(db_user)
     session.commit()
-    
+
     return {'message': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from fast_zero.schemas import UserPublic
 
 
@@ -5,3 +7,55 @@ def test_read_users_with_user(client, user):
     user_schema = UserPublic.model_validate(user).model_dump()
     response = client.get('/users/')
     assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'rod',
+            'email': 'rod@example.com',
+            'password': 'newtestpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'rod',
+        'email': 'rod@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client, user):
+    response = client.delete('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
+
+
+def test_put_not_found(client, user):
+    response = client.put(
+        '/users/999',
+        json={
+            'username': 'newuser',
+            'email': 'newemail@example.com',
+            'password': 'newpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_user_not_found(client, user):
+    response = client.delete('/users/999')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_get_user_by_id(client, user):
+    response = client.get(f'/users/{user.id}')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': user.id,
+        'username': user.username,
+        'email': user.email,
+    }
