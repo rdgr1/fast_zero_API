b09b39426d5e9a240fec966c71d1c502f5126316
Rodger Dias | BRASFORT <rodger.dias@brasfort.com.br>
2024-08-09 17:51:59 -0300

    Token jwt

--- fast_zero/security.py
+++ fast_zero/security.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 from jwt import encode
 from pwdlib import PasswordHash
 from zoneinfo import ZoneInfo
-from fastapi.security import OAuth2PasswordBearer(tokenUrl="token")
 
 pwd_context = PasswordHash.recommended()
 
@@ -31,9 +30,3 @@ def create_acess_token(data: dict):
     encoded_jwt = encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
-
-
-def get_current_user(
-    session: Session = Depends(get_session),
-    token: str = Depends(oauth2_scheme),
-):
